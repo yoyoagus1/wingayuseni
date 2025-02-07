31d2c6f86072e80ea36e69ce4554ed0f4afd7b47
yoyoagus1 <yoyoagus4@gmail.com>
2025-02-07 22:50:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.querySelector(".btn").addEventListener("click", function() {
     alert("Terima kasih telah membeli!");
 });
+.btn:hover {
+    background: #ff3300;
+    transition: 0.3s;
+}
